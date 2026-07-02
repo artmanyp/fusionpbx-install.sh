f09a30d9739662ccdc021450c7d9377a64d173b5
Artman YP <artman.yousefpour@gmail.com>
2026-07-02 09:22:14 +0330

    Support new freeswitch URL

--- ubuntu/resources/switch/source-master.sh
+++ ubuntu/resources/switch/source-master.sh
@@ -2,11 +2,14 @@
 echo "Installing the FreeSWITCH source"
 DEBIAN_FRONTEND=none APT_LISTCHANGES_FRONTEND=none apt-get install -y ntpdate libapache2-mod-log-sql-ssl libfreetype6-dev git-buildpackage doxygen yasm nasm gdb git build-essential automake autoconf 'libtool-bin|libtool' python uuid-dev zlib1g-dev 'libjpeg8-dev|libjpeg62-turbo-dev' libncurses5-dev libssl-dev libpcre3-dev libcurl4-openssl-dev libldns-dev libedit-dev libspeexdsp-dev libspeexdsp-dev libsqlite3-dev perl libgdbm-dev libdb-dev bison libvlc-dev libvlccore-dev vlc-nox pkg-config ccache libpng-dev libvpx-dev libyuv-dev libopenal-dev libbroadvoice-dev libcodec2-dev libflite-dev libg7221-dev libilbc-dev libmongoc-dev libsilk-dev libsoundtouch-dev libmagickcore-dev liblua5.2-dev libopus-dev libsndfile-dev libopencv-dev libavformat-dev libx264-dev erlang-dev libldap2-dev libmemcached-dev libperl-dev portaudio19-dev python-dev libsnmp-dev libyaml-dev libmp4v2-dev
 apt-get install -y unzip libpq-dev memcached libshout3-dev libvpx-dev libmpg123-dev libmp3lame-dev libpq-dev libvlc-dev
-
-apt-get update && apt-get install -y ntp curl haveged
-curl https://files.freeswitch.org/repo/deb/debian/freeswitch_archive_g0.pub | apt-key add -
-echo "deb http://files.freeswitch.org/repo/deb/freeswitch-1.6/ jessie main" > /etc/apt/sources.list.d/freeswitch.list
-echo "deb http://files.freeswitch.org/repo/deb/debian-unstable/ jessie main" > /etc/apt/sources.list.d/freeswitch.list
+#help: https://computingforgeeks.com/install-freeswitch-ubuntu/
+apt-get update && apt-get install -y ntp curl haveged gnupg2 lsb-release software-properties-common apt-transport-https
+#echo "your_signalwire_pat_here" > /tmp/patokoen
+TOKEN=$(cat /tmp/patokoen)
+curl --http1.1 -fsSL https://freeswitch.signalwire.com/repo/deb/debian-release/signalwire-freeswitch-repo.gpg | gpg --dearmor -o /usr/share/keyrings/signalwire-freeswitch-repo.gpg
+echo "machine freeswitch.signalwire.com login signalwire password $TOKEN" | tee /etc/apt/auth.conf.d/freeswitch.conf
+chmod 600 /etc/apt/auth.conf.d/freeswitch.conf
+echo "deb [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/debian-release/ bookworm main" | tee /etc/apt/sources.list.d/freeswitch.list
 apt-get update && apt-get upgrade -y
 apt-get install -y freeswitch-video-deps-most
 
